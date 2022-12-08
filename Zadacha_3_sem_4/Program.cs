9d10992830e51b1b6654f9fd80d73812714b5bfa
Basilio-one <vasya-w@yandex.ru>
2022-12-09 00:32:05 +0300

    Zadacha 3 ko-vo chisel

---
+++ Zadacha_3_sem_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountIndex (int arg)
+{
+    int count = 0;
+    while (arg>=1)
+    {
+        arg = arg/10;
+        count++;
+    } 
+    return count;
+}
+
+Console.WriteLine("Введите любое целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество чисел в {num} -> {CountIndex(num)}");
