d4213eadc6fde326592f9135a3ad019ae756c0b5
Basilio-one <vasya-w@yandex.ru>
2022-12-08 23:17:44 +0300

    rechenie zadaci 1

---
+++ Zadacha_sem_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+
+Console.WriteLine ("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i=1; i<=num;i++) sum+=i; // sum=sum+i
+
+Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {sum}");
