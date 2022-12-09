c36b8488bf1a029e56d0638096aafb3cdf504eea
Basilio-one <vasya-w@yandex.ru>
2022-12-09 19:59:08 +0300

    Zadacha 5 composition

---
+++ Zadacha_5_sem_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Numbers(int num)
+{
+    int com = 1;
+    for(int i = 1; i <= num; i++) 
+    com=com*i;
+    return com;
+}
+
+Console.WriteLine ("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех чисел от 1 до N  N = {num} -> {Numbers(num)}");
